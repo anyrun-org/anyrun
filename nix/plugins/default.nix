e973dc6e755a53078bf033a96a5c9aab2701bf34
n3oney <neo@neoney.dev>
2023-06-20 17:51:02 +0200

    fix: specify pname for plugins

--- nix/plugins/default.nix
+++ nix/plugins/default.nix
@@ -12,35 +12,38 @@
   name,
   lockFile,
   ...
-}:
-rustPlatform.buildRustPackage {
-  inherit name;
+}: let
+  cargoToml = builtins.fromTOML (builtins.readFile ../../plugins/${name}/Cargo.toml);
+in
+  rustPlatform.buildRustPackage {
+    pname = name;
+    name = cargoToml.package.name;
 
-  src = "${inputs.self}";
-  cargoLock.lockFile = lockFile;
+    src = "${inputs.self}";
+    cargoLock.lockFile = lockFile;
 
-  buildInputs = [
-    glib
-    atk
-    gtk3
-    librsvg
-    gtk-layer-shell
-  ];
+    buildInputs = [
+      glib
+      atk
+      gtk3
+      librsvg
+      gtk-layer-shell
+    ];
 
-  nativeBuildInputs = [
-    pkg-config
-    makeWrapper
-  ];
+    nativeBuildInputs = [
+      pkg-config
+      makeWrapper
+    ];
 
-  doCheck = true;
-  CARGO_BUILD_INCREMENTAL = "false";
-  RUST_BACKTRACE = "full";
-  copyLibs = true;
-  cargoBuildFlags = ["-p ${name}"];
+    doCheck = true;
+    CARGO_BUILD_INCREMENTAL = "false";
+    RUST_BACKTRACE = "full";
+    copyLibs = true;
+    cargoBuildFlags = ["-p ${name}"];
 
-  meta = with lib; {
-    description = "The ${name} plugin for Anyrun";
-    homepage = "https://github.com/Kirottu/anyrun";
-    license = with licenses; [gpl3];
-  };
-}
+    meta = with lib; {
+      description = "The ${name} plugin for Anyrun";
+      homepage = "https://github.com/Kirottu/anyrun";
+      license = with licenses; [gpl3];
+    };
+  }
