110852dfee7fb327d8a0f855728c4fb086c8f624
Mintori09 <tvmthtr@gmail.com>
2026-07-08 16:31:06 +0700

    ci(github): restore and update flake.nix, fix cargo-build to prevent test hangs

---
+++ flake.nix
@@ -0,0 +1,146 @@
+{
+  description = "A wayland native, highly customizable runner.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-parts,
+      systems,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ flake-parts.flakeModules.easyOverlay ];
+      systems = import systems;
+
+      perSystem =
+        {
+          self',
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          inherit (pkgs) callPackage;
+        in
+        {
+          packages =
+            let
+              lockFile = ./Cargo.lock;
+
+              # Since all plugin derivations are called with the exact same arguments
+              # it is possible to streamline calling packages with a single function
+              # that takes name as an argument, and handles default inherits.
+              mkPlugin =
+                name:
+                callPackage ./nix/packages/plugin.nix {
+                  inherit inputs lockFile;
+                  inherit name;
+                };
+            in
+            {
+              # By default the anyrun package is built without any plugins
+              # as per the `dontBuildPlugins` arg.
+              anyrun = callPackage ./nix/packages/anyrun.nix {
+                inherit inputs lockFile;
+              };
+
+              anyrun-with-all-plugins = self.packages.${system}.anyrun.override {
+                dontBuildPlugins = false;
+              };
+
+              # Expose each plugin as a separate package. This uses the mkPlugin function
+              # to call the same derivation with same default inherits and the name of the
+              # plugin every time.
+              applications = mkPlugin "applications";
+              dictionary = mkPlugin "dictionary";
+              kidex = mkPlugin "kidex";
+              nix-run = mkPlugin "nix-run";
+              randr = mkPlugin "randr";
+              rink = mkPlugin "rink";
+              shell = mkPlugin "shell";
+              stdin = mkPlugin "stdin";
+              symbols = mkPlugin "symbols";
+              translate = mkPlugin "translate";
+              websearch = mkPlugin "websearch";
+              niri-focus = mkPlugin "niri-focus";
+
+              default = self'.packages.anyrun;
+            };
+
+          # Set up an overlay from packages exposed by this flake
+          overlayAttrs = config.packages;
+
+          devShells = {
+            default = pkgs.mkShell {
+              inputsFrom = builtins.attrValues self'.packages;
+              packages = with pkgs; [
+                rustc
+                cargo
+
+                clippy
+                rustfmt
+                taplo
+              ];
+            };
+
+            nix = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                alejandra # formatter
+                statix # linter
+                deadnix # dead-code finder
+              ];
+            };
+          };
+
+          # Provides the default formatter for 'nix fmt', which will format the
+          # entire tree with Alejandra. The wrapper script is necessary due to
+          # changes to the behaviour of Nix, which now encourages wrappers for
+          # tree-wide formatting.
+          formatter = pkgs.writeShellApplication {
+            name = "nix3-fmt-wrapper";
+
+            runtimeInputs = [
+              pkgs.nixfmt
+              pkgs.fd
+              pkgs.taplo
+            ];
+
+            text = ''
+              # Find Nix files in the tree and format them with Alejandra
+              fd "$@" -t f -e nix -x nixfmt -q '{}'
+
+              # Same for TOML files, but with Taplo
+              fd "$@" -t f -e toml -x taplo fmt '{}'
+            '';
+          };
+
+          # Provides checks to be built an ran on 'nix flake check'. They can also
+          # be built individually with 'nix build' as described below.
+          checks = {
+            # Check if codebase is properly formatted.
+            # This can be initiated with `nix build .#checks.<system>.nix-fmt`
+            # or with `nix flake check`
+            nix-fmt = pkgs.runCommand "nix-fmt-check" { nativeBuildInputs = [ pkgs.nixfmt ]; } ''
+              nixfmt --check ${self} < /dev/null | tee $out
+            '';
+          };
+        };
+
+      flake = {
+        homeManagerModules = {
+          anyrun = import ./nix/modules/home-manager.nix self;
+          default = self.homeManagerModules.anyrun;
+        };
+      };
+    };
+}
